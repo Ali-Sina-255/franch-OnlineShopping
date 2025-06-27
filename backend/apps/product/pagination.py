bbcbe43a3933ab0ba509fac17e450f4cae7cb881
Ali Sina Sultani <alisinasultani255@gmail.com>
2025-06-27 13:28:15 +0430

    pagination is added

--- backend/apps/product/pagination.py
+++ backend/apps/product/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import LimitOffsetPagination, PageNumberPaginatio
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 100
 

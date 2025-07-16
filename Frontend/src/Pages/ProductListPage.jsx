d493101c84390f2c4f1cc292e99735fba3994524
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-07-16 12:18:14 +0430

    complete about page

--- Frontend/src/Pages/ProductListPage.jsx
+++ Frontend/src/Pages/ProductListPage.jsx
@@ -260,7 +260,7 @@ const ProductListPage = ({
                 </div>
               ) : (
                 <motion.div layout>
-                  <div className="grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
+                  <div className="grid grid-cols-1 gap-x-4 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-x-5">
                     <AnimatePresence>
                       {productsToShow.length > 0 ? (
                         productsToShow.map((product) => (
